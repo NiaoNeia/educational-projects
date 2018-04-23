a1ea65c6b68cda601622f769a6c6eb3d58545314
NiaoNeia <38665511+NiaoNeia@users.noreply.github.com>
2018-04-23 19:46:16 +0300

    Add files via upload

---
+++ func_vvod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOne
+{
+    class MainClass
+    {
+       public static int VvodChisla()
+        {
+            Console.Out.WriteLine("Vvedite chislo ");
+            int chislo;
+            while (!Int32.TryParse(Console.ReadLine(),out chislo))
+            {
+                Console.Out.WriteLine("Neverniy vvod, poprobuite eshe raz ");
+            }
+            return chislo;
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            Console.Out.WriteLine("Vi vveli {0}", VvodChisla());
+            Console.ReadLine();
+        }
+    }
+}
