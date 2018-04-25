b9e96d99dd69bdb4073cdf405380aa45a4982774
NiaoNeia <38665511+NiaoNeia@users.noreply.github.com>
2018-04-25 19:29:05 +0300

    Add files via upload

---
+++ experiment1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Vvod (out float result)
+        {
+            Console.WriteLine("Vvedite chislo ");
+            while (!float.TryParse(Console.ReadLine(), out result))
+                {
+                Console.WriteLine("Neverniy Vvod ");
+            }
+
+        }
+         
+        static void Main(string[] args)
+            
+        {
+            
+            Vvod(out float n);
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
